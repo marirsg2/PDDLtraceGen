0caf68f0e080d51bcb92309a391db4a3edac92ef
marirsg <marirsg2@gmail.com>
2020-11-30 12:53:56 -0700

    problem set generation code done

--- Gripper_pddl/Gripper_gen_test_problem_set.py
+++ Gripper_pddl/Gripper_gen_test_problem_set.py
@@ -1,11 +1,12 @@
 
 import subprocess
 import os
+import shutil
 import pickle
 import copy
 from enum import Enum
 
-number_problems = 14
+number_problems = 30
 keywords_before_solution = "Actual search time"
 keywords_after_solution = "Plan length"
 #---for making problem files
@@ -13,20 +14,37 @@ problem_gen_exec = "./gripper "
 #code to generate a random problem space
 merged_data = []
 
+problems_folder = "GripperProblemSet"
+solutions_folder = "GripperFDSolutionSet"
+
 # all_configs= [["-n 1", "-r 2","-o 5"],["-n 1", "-r 4","-o 5"],["-n 1", "-r 8","-o 5"],["-n 1", "-r 16","-o 5"],["-n 1", "-r 64","-o 5"]]
 # all_configs= [["-n 1", "-r 13","-o 10"],["-n 1", "-r 5","-o 10"],["-n 1", "-r 7","-o 7"],["-n 1", "-r 13","-o 5"],
 #               ["-n 1", "-r 3","-o 7"],["-n 1", "-r 9","-o 5"],["-n 1", "-r 5","-o 9"]]
 #todo I wonder if it learns better when you have a subset of settings varying only one dimension, and another varying only another dimension rather than mixing
-all_configs= [["-n 1", "-r 5","-o 5"],["-n 1", "-r 5","-o 10"],["-n 1", "-r 7","-o 7"],["-n 1", "-r 13","-o 5"],
-              ["-n 1", "-r 3","-o 7"],["-n 1", "-r 9","-o 5"],["-n 1", "-r 5","-o 9"]]
+all_configs= [["-n 1", "-r 2","-o 100"],["-n 1", "-r 5","-o 13"],["-n 1", "-r 7","-o 7"],["-n 1", "-r 13","-o 5"],
+              ["-n 1", "-r 3","-o 7"],["-n 1", "-r 9","-o 5"]]
 
 number_problems = int(number_problems/len(all_configs))
+
+#---clear the folders
+# for folder in [problems_folder,solutions_folder]:
+for folder in [solutions_folder]:
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
 #----------------------
 for problem_config in all_configs:
     for problem_idx in range(number_problems):
         print("problem_config = ", problem_config)
         dest_name_suffix = "_".join(problem_config).replace("-","").replace(" ","")+"_"+str(problem_idx+1)
-        dest_problem_file_name = "./GripperProblemSet/gripper_problems_" + dest_name_suffix + ".pddl"#this is where the problems problem file generator stores the problem.pddl file
+        dest_problem_file_name = "./" +problems_folder +"/gripper_problems_" + dest_name_suffix + ".pddl"#this is where the problems problem file generator stores the problem.pddl file
         #---for FD
         fast_downward_exec_loc = "~/Fastdownward/fast-downward.py"
         fd_alias = ""
@@ -36,9 +54,13 @@ for problem_config in all_configs:
         # fd_heuristic_config = "--heuristic \"hff=ff()\" --heuristic \"hcea=cea()\" --search \"lazy_greedy([hff, hcea], preferred=[hff, hcea])\""
         domain_file_loc = "./gripper_domain.pddl"
         problem_file_loc = dest_problem_file_name
-        solution_file_loc = "./GripperFDSolutionSet/gripper_solution_"+dest_name_suffix+ ".txt"#THIS Is where the solutions from FASTDDOWNWARD are stored, not the traces.
+        solution_file_loc = "./"+solutions_folder +  "/gripper_solution_"+dest_name_suffix+ ".txt"#THIS Is where the solutions from FASTDDOWNWARD are stored, not the traces.
 
         #---create problem files
+        # !!!!!!!!!!!!!!!!
+        # IMPORTANT note if you COMMENT out the following 2 lines, then it will reuse the problem file already there,
+        #  this allows you to make modifications to them. and see the assoc solutions
+        # make sure to NOT empty the problem folder, and only empty the solution folder
         command = problem_gen_exec + " ".join(problem_config)
         os.system(command +" > " + dest_problem_file_name)
 
